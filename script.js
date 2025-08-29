0751b660f87be4a8624acb98b7ec6b216c22e0d0
shmmdev <shmm.dev@gmail.com>
2025-08-29 21:34:20 +0600

    copy and call function added

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ const heartCountEl = document.querySelector(".heart-count");
 const coinCountEl = document.querySelector(".credit-count");
 const copyCountEl = document.querySelector(".copy-count");
 
+const heartReactionAllBtn = document.querySelectorAll(
+  ".card-love-react-container"
+);
+const copyNumberAllBtn = document.querySelectorAll(".btn-copy");
+const allCallBtn = document.querySelectorAll(".btn-call");
+
 const historyCardsContainerEl = document.querySelector(
   ".history-cards-container"
 );
@@ -16,8 +22,26 @@ const btnClearHistory = document.querySelector(".btn-clear-history");
 // Initialization
 function Init() {
   btnClearHistory.addEventListener("click", clearHistory);
-  clearHistory();
+  //   clearHistory();
+
+  //   Add Event Listener to all the heart icons
+  for (const heartReactBtn of heartReactionAllBtn) {
+    heartReactBtn.addEventListener("click", heartReactHandler);
+  }
+
+  //   Add Event Listener to all the copy buttons
+  for (const copyBtn of copyNumberAllBtn) {
+    copyBtn.addEventListener("click", () => copyNumberToClipboard(copyBtn));
+  }
+
+  //   Add Event Listener to all the Call buttons
+  for (const callBtn of allCallBtn) {
+    callBtn.addEventListener("click", () => callNumber(callBtn));
+  }
+
+  updateUI();
 }
+
 // Reset UI Function
 function resetUI() {
   heartCount = 0;
@@ -29,10 +53,50 @@ function resetUI() {
   copyCountEl.textContent = copyCount;
 }
 
+//  Update UI to current state function
+function updateUI() {
+  heartCountEl.textContent = heartCount;
+  coinCountEl.textContent = coinCount;
+  copyCountEl.textContent = copyCount;
+
+  // Call history adding function
+}
+
 // Clear History Function
 function clearHistory() {
   historyCardsContainerEl.innerHTML = "";
   resetUI();
+}
+
+// Heart Reaction Function
+function heartReactHandler() {
+  heartCount++;
+  updateUI();
+}
+
+// Copy Phone Number to Clipboard Function
+function copyNumberToClipboard(copyBtn) {
+  const cardEl = copyBtn.parentElement.parentElement;
+  const phoneNumber = cardEl.querySelector(".phone-number").textContent;
+  navigator.clipboard.writeText(phoneNumber);
+  copyCount++;
+  updateUI();
+
+  alert("📋 Number has been copied to clipboard: " + phoneNumber);
+}
+
+// Call an Emergency Number Function
+function callNumber(callBtn) {
+  if (coinCount >= 20) {
+    const cardEl = callBtn.parentElement.parentElement;
+    const phoneNumber = cardEl.querySelector(".phone-number").textContent;
+    const mainHeading = cardEl.querySelector(".card-heading").textContent;
+
+    coinCount -= 20;
+    updateUI();
+
+    alert("📞 Calling " + mainHeading + ": " + phoneNumber);
+  }
 }
 
 // Running first time
